b76482fc9f551af9e821e30e2c4153cab003819d
Daniela-q-m <dq2033@nyu.edu>
2022-01-10 22:06:01 -0500

    Bar Plot

--- Gene Expression Data.R
+++ Gene Expression Data.R
@@ -6,29 +6,22 @@ View(df1)
 
 
 
-# Sort by c3 and c4
+# Sort by most expressed genes
 df2 <-df1[order(-df1$UT1, -df1$UT2),]
 head(df2)
-
-
-gene_names<-df1[1]
-first_condition<-c(df1[3])
-second_condition<-c(df[4])
-third_condition<-c(df[5])
-fourth_condition<-c(df[6])
-fifth_condition<-c(df[7])
-sixth_condition<-c(df[8])
-
 
 
 library(plotly)
 
 fig <- plot_ly(
-  x = c(df2$gene_name[1:10]),
-  y = c(df2$UT1[1:10]),
-  name = "Group 1",
+  x = c(df2$gene_name[1:50]),
+  y = c(df2$UT1[1:50]),
+  name = "UT1",
   type = "bar"
 )
-fig <- fig %>% add_trace(y = df2$UT2[1:10], name = 'Group 2')
+fig <- fig %>% add_trace(y = df2$UT2[1:50], name = 'UT2', marker = list(color = 'rgb(204,204,204)'))
 
+fig <- fig %>% layout(title = "Gene Expression Data",
+                      xaxis = list(title = "Gene Names"),
+                      yaxis = list(title = "Gene Counts"))
 fig
